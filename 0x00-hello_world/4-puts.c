e6d7b4346d14e8c48ac110563bdc4b26afc3ee38
Benewaa <mollydadzie@gmail.com>
2023-02-20 15:52:18 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Bravo)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
