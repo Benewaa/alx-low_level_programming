0c0e8ad18a5480f49f0b0af327eb438e7d50357a
Benewaa <mollydadzie@gmail.com>
2023-02-20 16:23:46 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the function print f
+ * Return: 0 (Bravo)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
